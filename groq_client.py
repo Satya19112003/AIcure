2b49a3eeb6240fd2c784c0372ce6cdd84bb46ab5
Satya19112003 <ukalasapudi@gmail.com>
2025-10-21 15:35:05 +0530

    Update groq_client.py

--- groq_client.py
+++ groq_client.py
@@ -5,13 +5,11 @@ def analyze_with_groq(query, encoded_image=None, model="llama-3.2-11b-vision-pre
     try:
         client = Groq(api_key=GROQ_API_KEY)
         messages = [{"role": "user", "content": [{"type": "text", "text": query}]}]
-
         if encoded_image:
             messages[0]["content"].append({
                 "type": "image_url",
                 "image_url": {"url": f"data:image/jpeg;base64,{encoded_image}"}
             })
-
         chat_completion = client.chat.completions.create(messages=messages, model=model)
         return chat_completion.choices[0].message.content
     except Exception as e:
