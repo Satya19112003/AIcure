3c8cf151f2ad59ffe2fae8693759b49f8cf6eda2
Satya19112003 <ukalasapudi@gmail.com>
2025-11-21 13:03:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # Add your Groq API key here
-GROQ_API_KEY = "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"
+GROQ_API_KEY = "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"
+
 
